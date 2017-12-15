cf450b2e9675e3d3ee32531817a86b16b969218a
rbryan21 <rbryan21@gmail.com>
2017-12-14 23:38:17 -0500

    Completing assignment for adding UnitOfMeasure

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -1,6 +1,7 @@
 package guru.springframework.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -18,6 +19,12 @@ public class Recipe {
     private String directions;
     // todo add
     // private Difficulty difficulty;
+
+    // we're saying that Recipe owns the ingredients and
+    // that this recipe will be stored on the set of ingredients as a property called recipe (in Ingredient.java)
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+
     // creates a BLOB field in the data base
     @Lob
     private Byte[] image;
