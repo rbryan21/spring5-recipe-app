e2775755f0fa331d95231850f5eadcb25ef6c58d
rbryan21 <rbryan21@gmail.com>
2017-12-16 01:32:13 -0500

    Adding repository interfaces for category, recipe, and unit of measure. Creating category and difficulty domain classes. Creating ManyToMany mapping between Recipe and Category.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -17,8 +17,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // todo add
-    // private Difficulty difficulty;
 
     // we're saying that Recipe owns the ingredients and
     // that this recipe will be stored on the set of ingredients as a property called recipe (in Ingredient.java)
@@ -33,6 +31,15 @@ public class Recipe {
     // if this recipe is deleted, the corresponding note is deleted
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
+
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -113,4 +120,29 @@ public class Recipe {
     public void setNote(Note note) {
         this.note = note;
     }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
+
 }
